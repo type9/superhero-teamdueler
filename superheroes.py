5dc69735b9164c3d302c6d13670f97f48ac94062
type9 <gabespersonal@gmail.com>
2019-09-26 16:07:45 -0700

    Added Team class. Fixed Hero class errors. Added team_test.py file

--- superheroes.py
+++ superheroes.py
@@ -1,5 +1,7 @@
 import random
 
+DEBUG = True # var for enabling console logs for debugging
+
 class Ability:
     def __init__(self, name, attack_strength):
         self.name = name
@@ -7,6 +9,12 @@ class Ability:
 
     def attack(self):
         return random.randint(0, self.max_damage)
+
+class Weapon(Ability):
+    def attack(self):
+        if DEBUG:
+            print(random.randint((self.max_damage/2), self.max_damage))
+        return random.randint((self.max_damage/2), self.max_damage)
 
 class Armor:
     def __init__(self, name, max_block):
@@ -17,12 +25,14 @@ class Armor:
         return random.randint(0, self.max_block)
 
 class Hero:
-    def __init__(self, name, start_health):
+    def __init__(self, name, start_health=100):
         self.abilities = list()
         self.armors = list()
         self.name = name
         self.starting_health = start_health
         self.current_health = start_health
+        self.deaths = 0
+        self.kills = 0
 
     def add_ability(self, ability):
         self.abilities.append(ability)
@@ -53,7 +63,7 @@ class Hero:
         return True
     
     def fight(self, opponent):
-        turn = 1 # controls who is attacking each iteration
+        turn = 0 # controls who is attacking each iteration
         while self.is_alive() and opponent.is_alive():
             turn += 1 # because it starts at 2, self will always attack first
             if (turn % 2) == 0:
@@ -69,3 +79,42 @@ class Hero:
             print(f'Opponent {opponent.name} is victorious')
         else:
             print(f'Hero {self.name} is victorious')
+    
+    def add_kill(self, num_kills):
+        self.kills += num_kills
+    
+    def add_death(self, num_deaths):
+        self.deaths += num_deaths
+
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.heroes = list()
+    
+    def remove_hero(self, name):
+        for hero in range(len(self.heroes)):
+            if self.heroes[hero].name == name:
+                self.heroes.pop(hero)
+                return
+        return 0
+    
+    def view_all_heroes(self):
+        for hero in self.heroes:
+            print(hero.name)
+
+    def add_hero(self, hero):
+        self.heroes.append(hero)
+
+    def attack(self, other_team):
+        attacker = self.heroes[random.randint(0, len(self.heroes - 1))]
+        defender = other_team.heroes[random.randint(0, len(other_team.heroes - 1))]
+        attacker.fight(defender)
+    
+    def revive_heroes(self):
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+
+    def stats(self):
+        for hero in self.heroes:
+            kdr = hero.kills / hero.deaths
+            print(f'{hero.name}: {kdr} KDR')
