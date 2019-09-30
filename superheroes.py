33f844bf8c69470017a81d6d8cb2764426ad2fc8
type9 <gabespersonal@gmail.com>
2019-09-30 09:34:56 -0700

    Final push for grading

--- superheroes.py
+++ superheroes.py
@@ -243,8 +243,26 @@ class Arena:
         self.team_two.print_team_status()
 
 if __name__ == "__main__":
+    game_is_running = True
+
+    # Instantiate Game Arena
     arena = Arena()
+
+    #Build Teams
     arena.build_team_one()
     arena.build_team_two()
-    arena.team_battle()
-    arena.show_stats()
+
+    while game_is_running:
+
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
+
+        #Check for Player Input
+        if play_again.lower() == "n":
+            game_is_running = False
+
+        else:
+            #Revive heroes to play again
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
